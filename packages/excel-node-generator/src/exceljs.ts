0340fe12b7c63f53411ccee9eb0496b2f2cf1797
wx-chevalier <wxchevalier925@gmail.com>
2020-10-15 14:41:57 +0800

    feat: update articles or snippets

--- packages/excel-node-generator/src/exceljs.ts
+++ packages/excel-node-generator/src/exceljs.ts
@@ -105,103 +105,107 @@ export async function fillSheet(
   sheetDO: Partial<WorksheetDO>,
   workbook: Workbook,
 ) {
-  for (const rowDO of sheetDO.rows) {
-    if (!rowDO.number) {
-      console.error('>>>fillSheet>>>', rowDO, '>>>invalid number');
-      continue;
-    }
+  if (isValidArray(sheetDO.rows)) {
+    for (const rowDO of sheetDO.rows) {
+      if (!rowDO.number) {
+        console.error('>>>fillSheet>>>', rowDO, '>>>invalid number');
+        continue;
+      }
 
-    const $row = sheet.getRow(rowDO.number);
+      const $row = sheet.getRow(rowDO.number);
 
-    // 判断是否需要隐藏
-    if (rowDO.hidden) {
-      $row.hidden = rowDO.hidden;
+      // 判断是否需要隐藏
+      if (rowDO.hidden) {
+        $row.hidden = rowDO.hidden;
+      }
     }
   }
 
-  // 遍历全部的 Cell
-  for (const cellDO of sheetDO.cells) {
-    try {
-      let mergableCellAddress = `${cellDO.address}:${cellDO.address}`;
+  if (isValidArray(sheetDO.cells)) {
+    // 遍历全部的 Cell
+    for (const cellDO of sheetDO.cells) {
+      try {
+        let mergableCellAddress = `${cellDO.address}:${cellDO.address}`;
 
-      // 首先判断是否需要合并
-      if (cellDO.mergedCellAddress) {
-        mergableCellAddress = `${cellDO.address}:${cellDO.mergedCellAddress}`;
-        sheet.mergeCells(mergableCellAddress);
+        // 首先判断是否需要合并
+        if (cellDO.mergedCellAddress) {
+          mergableCellAddress = `${cellDO.address}:${cellDO.mergedCellAddress}`;
+          sheet.mergeCells(mergableCellAddress);
+        }
+
+        const $cell = sheet.getCell(cellDO.address);
+
+        // 添加数据校验
+        if (cellDO.dataValidation) {
+          $cell.dataValidation = cellDO.dataValidation;
+        }
+
+        // 然后依次填充内容
+        if (cellDO.style) {
+          mergeStyle($cell, cellDO.style);
+        }
+
+        switch (cellDO.type) {
+          case CellValueType.Null:
+            break;
+          case CellValueType.Merge:
+            break;
+          case CellValueType.Number:
+            $cell.value = cellDO.value as number;
+            break;
+          case CellValueType.String:
+            $cell.value = cellDO.value as string;
+            break;
+          case CellValueType.Date:
+            $cell.value = new Date(cellDO.value as string);
+            break;
+          case CellValueType.Hyperlink:
+            $cell.value = cellDO.value as CellHyperlinkValue;
+            break;
+          case CellValueType.RichText:
+            $cell.value = cellDO.value as CellRichTextValue;
+            break;
+          case CellValueType.Boolean:
+            $cell.value = cellDO.value as boolean;
+            break;
+          case CellValueType.Qrcode:
+            const qrcodeValue = cellDO.value as CellQrcodeValue;
+
+            try {
+              const base64 = await QRCode.toDataURL(qrcodeValue.qrcodeText);
+              const imageId = workbook.addImage({ base64, extension: 'png' });
+              sheet.addImage(imageId, mergableCellAddress);
+            } catch (_) {
+              console.error(
+                '>>>fillSheet>>>CellValueType.Qrcode>>>',
+                qrcodeValue.qrcodeText,
+              );
+            }
+
+            break;
+          case CellValueType.Image:
+            const imageValue = cellDO.value as CellImageValue;
+
+            try {
+              // 抓取图片
+              const base64 = await getImageAsBase64(imageValue.src);
+              const imageId = workbook.addImage({ base64, extension: 'png' });
+              sheet.addImage(imageId, mergableCellAddress);
+            } catch (_) {
+              console.error(
+                '>>>fillSheet>>>CellValueType.Image>>>',
+                imageValue.src,
+              );
+            }
+
+            break;
+          default:
+            $cell.value = cellDO.value as any;
+            break;
+        }
+      } catch (_) {
+        console.error('>>>fillSheet>>>cell', _);
       }
-
-      const $cell = sheet.getCell(cellDO.address);
-
-      // 添加数据校验
-      if (cellDO.dataValidation) {
-        $cell.dataValidation = cellDO.dataValidation;
-      }
-
-      // 然后依次填充内容
-      if (cellDO.style) {
-        mergeStyle($cell, cellDO.style);
-      }
-
-      switch (cellDO.type) {
-        case CellValueType.Null:
-          break;
-        case CellValueType.Merge:
-          break;
-        case CellValueType.Number:
-          $cell.value = cellDO.value as number;
-          break;
-        case CellValueType.String:
-          $cell.value = cellDO.value as string;
-          break;
-        case CellValueType.Date:
-          $cell.value = new Date(cellDO.value as string);
-          break;
-        case CellValueType.Hyperlink:
-          $cell.value = cellDO.value as CellHyperlinkValue;
-          break;
-        case CellValueType.RichText:
-          $cell.value = cellDO.value as CellRichTextValue;
-          break;
-        case CellValueType.Boolean:
-          $cell.value = cellDO.value as boolean;
-          break;
-        case CellValueType.Qrcode:
-          const qrcodeValue = cellDO.value as CellQrcodeValue;
-
-          try {
-            const base64 = await QRCode.toDataURL(qrcodeValue.qrcodeText);
-            const imageId = workbook.addImage({ base64, extension: 'png' });
-            sheet.addImage(imageId, mergableCellAddress);
-          } catch (_) {
-            console.error(
-              '>>>fillSheet>>>CellValueType.Qrcode>>>',
-              qrcodeValue.qrcodeText,
-            );
-          }
-
-          break;
-        case CellValueType.Image:
-          const imageValue = cellDO.value as CellImageValue;
-
-          try {
-            // 抓取图片
-            const base64 = await getImageAsBase64(imageValue.src);
-            const imageId = workbook.addImage({ base64, extension: 'png' });
-            sheet.addImage(imageId, mergableCellAddress);
-          } catch (_) {
-            console.error(
-              '>>>fillSheet>>>CellValueType.Image>>>',
-              imageValue.src,
-            );
-          }
-
-          break;
-        default:
-          $cell.value = cellDO.value as any;
-          break;
-      }
-    } catch (_) {
-      console.error('>>>fillSheet>>>cell', _);
     }
   }
 }
